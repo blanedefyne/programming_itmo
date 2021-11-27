84cd0ce18afcd67069265080a658068b3964ca8f
Pavel Zelenin <89858956+murd0ck8@users.noreply.github.com>
2021-11-27 11:09:49 +0300

    Update word_count.c

--- word_count.c
+++ word_count.c
@@ -11,24 +11,27 @@ int main(int argc, char* argv[]){
     FILE *file;
     file = fopen(argv[2], "r");
 
-    if (argc < 3){
-        if (strings_compare(argv[1], "EXIT"))
+    if (argc < 3){                              /* проверка на корректный ввод */
+        if (strings_compare(argv[1], "EXIT"))   /* посчитав все нужное, можно завершить программу, написав EXIT */
             exit(0);
         else
             printf("Invalid input");
     }
-    else
+else {
         if ((strings_compare(argv[1], "-l")) || (strings_compare(argv[1], "--lines")))
             printf("%d", counter(file, "lines"));
         else if ((strings_compare(argv[1], "-w")) || (strings_compare(argv[1], "--words")))
             printf("%d", counter(file, "words"));
         else if ((strings_compare(argv[1], "-c")) || (strings_compare(argv[1], "--bytes")))
             printf("%d", counter(file, "bytes"));
+}
+    
+return 0;    
 
 }
 
 
-int strings_compare(char *first, char *second){
+int strings_compare(char *first, char *second){                  /*функция, сравнивающая строки - возвращает 1, если строки равны, иначе 0 */
     for (int i = 0 ;first[i] != '\0' || second[i] != '\0'; ++i){
         if (first[i] != second[i]) {
             return 0;
@@ -37,10 +40,10 @@ int strings_compare(char *first, char *second){
     return 1;
 }
 
-int counter(FILE *file, char *what_to_count){
-    int ch, words, bytes, lines;
+int counter(FILE *file, char *what_to_count){                    /* функция counter, считающая сразу все (байты, строки, слова)*/
+    int ch, words, bytes, lines;                                 /* но выводящая только то, что нужно (2-ой параметр) */
     words = lines = bytes = 0;
-    int flag = -1; /*чтобы не срабатывала 67 строка */
+    int flag = -1;   /*чтобы не срабатывала 67 строка */
 
 
     while ((ch = fgetc(file)) != EOF){
